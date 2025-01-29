d0d4a10d8d9b9a3266cda18a1d76cb847903e6a3
ibrahimhand <ibramhand555@gmail.com>
2025-01-29 13:52:29 +0100

    params by user id

--- models/user_subscription.go
+++ models/user_subscription.go
@@ -3,10 +3,12 @@ package models
 import "time"
 
 type UserSubscription struct {
-    ID             int       `json:"id"`
-    UserID         int       `json:"user_id"`
-    SubscriptionID int       `json:"subscription_id"`
-    CreatedAt      time.Time `json:"created_at"`
-    UpdatedAt      time.Time `json:"updated_at"`
-    DeletedAt      time.Time `json:"deleted_at"`
+	ID               int        `json:"id"`
+	UserID           int        `json:"user_id"`
+	SubscriptionID   int        `json:"subscription_id"`
+	SubscriptionName string     `json:"name"`
+	ProductID        int        `json:"product_id"`
+	CreatedAt        time.Time  `json:"created_at"`
+	UpdatedAt        time.Time  `json:"updated_at"`
+	DeletedAt        *time.Time `json:"deleted_at"`
 }
